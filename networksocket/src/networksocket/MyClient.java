d12f57e1b566c34bfddef33d3d820d5f06e61c72
Waseem5353 <81692594+Waseem5353@users.noreply.github.com>
2021-05-01 17:39:52 +0500

    Add files via upload
    Q1.network socket

---
+++ networksocket/src/networksocket/MyClient.java
@@ -0,0 +1,28 @@
+
+package networksocket;
+
+import java.io.DataOutputStream;
+import java.net.Socket;
+
+public class MyClient {
+    public static void main(String[] args) {
+        try{
+            
+            Socket s = new Socket("localhost", 786);
+            
+            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+            
+            dout.writeUTF("Hello waseem");
+            dout.writeUTF("mid paper");
+            
+            dout.flush();
+            dout.close();
+            s.close();
+            
+            
+        }
+        catch(Exception ex){
+            ex.printStackTrace();
+        }
+    }
+}
